097fcd586350588af34fd5eb87a64628dc2f2fe7
Josh Gough <jsgough@gmail.com>
2019-06-13 10:41:49 -0400

    Support inverting the collapsed card selections
    This makes it easier to report on what folks spoke about in standup when you go to scrum of scrums

--- contents.js
+++ contents.js
@@ -390,10 +390,26 @@ function run(GM_xmlhttpRequest, GM_addStyle, secrets) {
     });
   }
 
+  /*
+    Invert collapsed cards so that you can speak to only the items that folks talked about in stand-up
+  */
+  function initializeCollapsibleCardsToggleInvert() {
+    const invertCollapsedCards = $('<input type="checkbox" title="Invert collapsed card selections" />');
+    invertCollapsedCards.on("change", () =>
+      $('.card-toggle').each(() => {
+        $(this).prop('checked', !$(this).prop('checked'));
+      });
+    );
+    
+    $(selectors.board)
+      .parent()
+      .prepend(invertCollapsedCards);
+  }
+  
   function initializeCollapsibleCards() {
     const story = $(this);
     const aging = story.find(".aging");
-    const toggleDetails = $('<input type="checkbox" />');
+    const toggleDetails = $('<input type="checkbox" class="card-toggle" />');
     toggleDetails.on("change", () =>
       story.find(".title, .bottom-content").toggle()
     );
@@ -412,10 +428,12 @@ function run(GM_xmlhttpRequest, GM_addStyle, secrets) {
   $(document).arrive(selectors.card, initializeCopyToClipboard);
   $(document).arrive(selectors.card, initializeCardAging);
   $(document).arrive(selectors.card, initializeCustomIcons);
+  $(document).arrive(selectors.board, initializeCollapsibleCardsToggleInvert);  
   $(document).arrive(selectors.card, initializeCollapsibleCards);
   $(document).arrive(selectors.sidepanelTabs, intializeBuildStream);
   $(document).arrive(selectors.board, initializeListView);
-  $(document).arrive(selectors.swimlanes, initializeCollapsableSwimlanes);
-  $(document).arrive(selectors.columnHeader, initializeCollapsableColumns);
+  $(document).arrive(selectors.swimlanes, initializeCollapsibleSwimlanes);
+  $(document).arrive(selectors.columnHeader, initializeCollapsibleColumns);
   $(document).arrive(selectors.board, initalizeMilestoneBanner);
+  
 }
